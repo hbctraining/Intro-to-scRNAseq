0f85bd7edca7631ec7ba05ceb8d13ab7b1b3eb8b
Gammerdinger <wjgdinger@gmail.com>
2026-03-03 14:29:33 -0500

    Updated download script and download for participants

--- download.sh
+++ download.sh
@@ -18,14 +18,6 @@ rm "$ZIPFILE"
 rm -rf __MACOSX
 rm -rf single_cell_rnaseq
 
-# Web summary
-curl -L "https://www.dropbox.com/scl/fi/skjyyvs9078pbef7s4qvb/ctrl_web_summary.html?rlkey=335gw4qmy29uny5813wg0zakr&dl=1" -o "lessons/data/ctrl_web_summary.html"
-curl -L "https://www.dropbox.com/scl/fi/r6n83y57cd7vhc130zm1g/stim_web_summary.html?rlkey=50ur193yfsy5ex9urkmr87h3c&dl=1" -o "lessons/data/stim_web_summary.html"
-
-# Metrics summary
-curl -L "https://www.dropbox.com/scl/fi/qnz44ng51ojmhu44acc8g/ctrl_metrics_summary.csv?rlkey=8zx5g1mtn6mrlwpv0syoz3bv4&st=9d0okyhf&dl=1" -o "lessons/data/ctrl_metrics_summary.csv"
-curl -L "https://www.dropbox.com/scl/fi/gaa4sabcbhwqpuogfh0us/stim_metrics_summary.csv?rlkey=h2uokc9r2edeinxtzgrpw3ds4&dl=1" -o "lessons/data/stim_metrics_summary.csv"
-
 # merged_seurat object
 curl -L "https://www.dropbox.com/scl/fi/pj13sjx0fkaxgflf3wy7n/merged_seurat.RDS?rlkey=0sk9cv33j2mq9e6uxbf0ixgi9&st=w6px5kj1&dl=1" -o "lessons/data/merged_seurat.RDS"
 
